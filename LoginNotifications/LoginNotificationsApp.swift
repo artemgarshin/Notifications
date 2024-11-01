222f67295e2cd50c0d2f7e445cd1e8da0bdf9915
artemgarshin <garshinareem@gmail.com>
2024-11-01 15:27:40 +0300

    Initial Commit

---
+++ LoginNotifications/LoginNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginNotificationsApp.swift
+//  LoginNotifications
+//
+//  Created by Артем Гаршин on 01.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
